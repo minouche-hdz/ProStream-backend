f64630e188364d2c90d915b17e5cab0efecd4da4
minouche <emilien.hernandez@etudiant.univ-reims.fr>
2025-11-25 12:02:40 +0100

    feat: Configure CORS and expose application to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true, // Permet toutes les origines pour le développement. À ajuster pour la production.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('ProStream Backend API')
@@ -14,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
